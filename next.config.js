b1cbc81f68a359e99940131a0310269200d703d8
billjhlee <billjhlee@gmail.com>
2021-08-29 02:36:45 +0900

    refactor: config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,36 @@
-/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 const linguiConfig = require('./lingui.config.js');
 
 const { locales, sourceLocale } = linguiConfig;
 
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: [
-      'res.cloudinary.com',
-      'raw.githubusercontent.com',
-      'logos.covalenthq.com',
-    ],
-  },
-  i18n: {
-    locales,
-    defaultLocale: sourceLocale,
-  },
-};
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    pwa: {
+      dest: 'public',
+      runtimeCaching,
+      disable: process.env.NODE_ENV === 'development',
+    },
+    images: {
+      domains: [
+        'assets.sushi.com',
+        'res.cloudinary.com',
+        'raw.githubusercontent.com',
+        'logos.covalenthq.com',
+      ],
+    },
+    reactStrictMode: true,
+    i18n: {
+      locales,
+      defaultLocale: sourceLocale,
+    },
+  }),
+);
+
+// Don't delete this console log, useful to see the config in Vercel deployments
+console.log('next.config.js', JSON.stringify(module.exports, null, 2));
