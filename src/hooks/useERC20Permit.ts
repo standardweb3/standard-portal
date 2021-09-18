57e4d2626505ec705272d439b08ed994322d312e
Hyungsuk Kang <hskang9@github.com>
2021-09-18 22:31:18 +0900

    update eip712 domain

--- src/hooks/useERC20Permit.ts
+++ src/hooks/useERC20Permit.ts
@@ -293,7 +293,7 @@ export function useERC20Permit(
 
 const REMOVE_V2_LIQUIDITY_PERMIT_INFO: PermitInfo = {
   version: '1',
-  name: 'SushiSwap LP Token',
+  name: 'Standard LTR Token',
   type: PermitType.AMOUNT,
 };
 
