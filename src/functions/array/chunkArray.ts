6952ca3d1140ba51623079b473bbbdad6e7bfdf6
Hyungsuk Kang <hskang9@github.com>
2021-09-29 09:25:59 +0900

    fix gas limit error due to too low gas limit from frontier

--- src/functions/array/chunkArray.ts
+++ src/functions/array/chunkArray.ts
@@ -1,5 +1,5 @@
 const CONSERVATIVE_BLOCK_GAS_LIMIT = 10_000_000 // conservative, hard-coded estimate of the current block gas limit
-export const DEFAULT_GAS_REQUIRED = 200_000 // the default value for calls that don't specify gasRequired
+export const DEFAULT_GAS_REQUIRED = 20_000 // the default value for calls that don't specify gasRequired
 
 // chunks array into chunks
 // evenly distributes items among the chunks
