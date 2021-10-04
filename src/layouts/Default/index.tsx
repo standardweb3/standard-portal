7ed697bd55f89eb7d8de56d7c4cc8b328579a4b1
billjhlee <billjhlee@gmail.com>
2021-10-05 06:21:35 +0900

    adjust mobile padding

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -26,9 +26,8 @@ const Layout = ({ children }) => {
           {`
             .mobile-pad {
               padding: env(safe-area-inset-top, 20px)
-                env(safe-area-inset-right, 20px)
-                env(safe-area-inset-bottom, 20px)
-                env(safe-area-inset-left, 20px);
+                env(safe-area-inset-right, 0px)
+                env(safe-area-inset-bottom, 20px) env(safe-area-inset-left, 0px);
             }
           `}
         </style>
