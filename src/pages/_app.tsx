8d43fb418fcde9e1c5a607e9f674f9553834a958
billjhlee <billjhlee@gmail.com>
2021-10-08 15:29:18 +0900

    fix: app title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -110,7 +110,7 @@ function MyApp({
           content="Standard Protocol, building the next multichain money"
         />
 
-        <meta name="application-name" content="Standard Protocol App" />
+        <meta name="application-name" content="Standard" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Standard" />
@@ -122,7 +122,7 @@ function MyApp({
         <meta name="theme-color" content="#F338C3" />
 
         <meta key="twitter:card" name="twitter:card" content="app" />
-        <meta key="twitter:title" name="twitter:title" content="Standard Protocol App" />
+        <meta key="twitter:title" name="twitter:title" content="Standard" />
         <meta key="twitter:url" name="twitter:url" content="https://apps/standard.tech" />
         <meta
           key="twitter:description"
@@ -132,7 +132,7 @@ function MyApp({
         {/* <meta key="twitter:image" name="twitter:image" content="https://app.sushi.com/icons/icon-192x192.png" /> */}
         <meta key="twitter:creator" name="twitter:creator" content="@standarddefi" />
         <meta key="og:type" property="og:type" content="website" />
-        <meta key="og:site_name" property="og:site_name" content="Standard Protocol App" />
+        <meta key="og:site_name" property="og:site_name" content="Standard" />
         <meta key="og:url" property="og:url" content="https://apps.standard.tech" />
         {/* <meta key="og:image" property="og:image" content="https://app.sushi.com/apple-touch-icon.png" /> */}
         <meta
