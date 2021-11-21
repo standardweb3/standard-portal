fc46505087272746c275ecd73f06b94f0ae184e6
billjhlee <billjhlee@gmail.com>
2021-11-22 05:19:45 +0900

    rebuild

--- src/bridge/functions/toNormalToken.ts
+++ src/bridge/functions/toNormalToken.ts
@@ -1,4 +1,5 @@
 import {
+  AnyswapCurrency,
   AnyswapToken,
   NATIVE,
   Token,
@@ -18,7 +19,7 @@ export function toNormalToken(token: AnyswapToken | undefined, chainId): Token {
 }
 
 export function toNormalCurrency(
-  token: AnyswapToken | undefined,
+  token: AnyswapCurrency | undefined,
   chainId,
 ): Token {
   if (!token) return undefined;
