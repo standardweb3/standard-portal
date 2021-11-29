9bef57f5acb1328799b71707d3daa47022dfe6f1
billjhlee <billjhlee@gmail.com>
2021-11-29 20:48:12 +0900

    analytics

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -247,7 +247,7 @@ export const ANALYTICS_URL: { [chainId in ChainId]?: string } = {
   [ChainId.XDAI]: 'https://analytics-xdai.sushi.com',
   [ChainId.HARMONY]: 'https://analytics-harmony.sushi.com',
   [ChainId.ARBITRUM]: undefined,
-  [ChainId.METIS]: undefined,
+  [ChainId.METIS]: 'https://metis.analytics.standard.tech',
 };
 
 export const EIP_1559_ACTIVATION_BLOCK: { [chainId in ChainId]?: number } = {
