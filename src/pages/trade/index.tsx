5656b4c0ac28fb7d5f2328bbe81d9e5e8762f382
billjhlee <billjhlee@gmail.com>
2022-01-07 03:53:46 +0900

    remove log

--- src/pages/trade/index.tsx
+++ src/pages/trade/index.tsx
@@ -104,7 +104,6 @@ function Tokens() {
     return (
       tokens
         ?.map((token) => {
-          console.log(token);
           const oneDayToken = oneDayTokens?.find(({ id }) => token.id === id);
           const priceYesterday = oneDayToken
             ? oneDayToken.derivedETH * oneDayEthPrice
