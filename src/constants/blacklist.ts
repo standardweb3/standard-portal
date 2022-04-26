f39de700299283c560b6a495b4afb0d31533f612
billjhlee <billjhlee@gmail.com>
2022-04-27 01:27:29 +0900

    add mdoge to metis trade blacklist

--- src/constants/blacklist.ts
+++ src/constants/blacklist.ts
@@ -8,4 +8,7 @@ export const TRADE_BLACKLIST = {
     '0x0918d629c2c4e9527bb093eba0aeb0204bdf1532': true,
     '0x6aaf863f8822441c5e23596af00a9c484a064341': true,
   },
+  [ChainId.METIS]: {
+    '0x31021e0b1cc6f89e04586a0e3cf5c46d5498a5e9': true
+  }
 };
