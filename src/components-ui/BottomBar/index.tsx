6c59ce381e716548897935bc8a857a849aa4e50b
billjhlee <billjhlee@gmail.com>
2022-05-01 20:38:57 +0900

    switchere sandbox

--- src/components-ui/BottomBar/index.tsx
+++ src/components-ui/BottomBar/index.tsx
@@ -2,9 +2,11 @@ import Switchere from '../Switchere';
 
 export function BottomBar() {
   return (
-    <div className="absolute bottom-0 z-10 w-full">
+    <div className="absolute bottom-0 w-full">
       <div className="w-full flex justify-end pb-4 px-4">
-        <Switchere />
+        <div className="z-10">
+          <Switchere />
+        </div>
       </div>
     </div>
   );
